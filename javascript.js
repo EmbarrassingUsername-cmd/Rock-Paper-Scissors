211e69cc4134359f26a0fc30f8c5b06dfbb55046
Jamie Bukraba <Jamie.Bukraba@googlemail.com>
2021-01-22 00:12:18 +0000

    5 round game function completed fixed error in computer rock choice

--- javascript.js
+++ javascript.js
@@ -1,41 +1,63 @@
+let playerWin=0,
+    computerWin=0,
+    tieGame=0
+    totalRoundNum=5
+game()
 
-
-
-function game(){}
-    for (i=0, i<totalRoundNum,i++){
-        playRound
+function game(){
+    for (i=0; i<totalRoundNum;i++){
+        const playerSelection = toTitleCase(prompt("Rock Paper or Scissors?"));
+        const computerSelection = computerPlay();
+        let result=playRound(playerSelection,computerSelection)
+        if (result=="playerwin"){
+            playerWin=++playerWin
+        }
+        else if(result="computerwin"){
+            computerWin=++computerWin
+        }
+        else{
+            tieGame=++tieGame
+        }
 
     }
-function playRound(){
-    const playerSelection = toTitleCase(prompt("Rock Paper or Scissors?"));
-    const computerSelection = computerPlay();
+    if(playerWin>computerWin){
+        console.log("Congratulations you win! " + playerWin + " win(s) " + computerWin + "loss(es) and " + tieGame +" tie(s). Let's see if you can keep it up!");
+    }
+    if(playerWin<computerWin){
+        console.log("Sorry you lose " + playerWin + " win(s) " + computerWin + " loss(es) and " + tieGame + " tie(s). Better luck next time");
+    }
+    else{
+        console.log("It's a tie" + playerWin + " win(s) " + computerWin + " loss(es) and " + tieGame + " tie(s) why not play again?" );
+    }
+}
+function playRound(playerSelection,computerSelection){
     let gameOutcome=playerSelection+computerSelection
     if (gameOutcome=="RockPaper"){console.log("You Lose! " + computerSelection + " beats " + playerSelection); 
-    return computerwin}
+    return "computerwin"}
 
     else if (gameOutcome=="RockScissors"){console.log("You Win! " + computerSelection + " beats " + playerSelection);
-    return playerwin}
+    return "playerwin"}
 
     else if(gameOutcome=="RockRock"){console.log("It's A tie you both picked " +computerSelection);
-    return tie}
+    return "tie"}
 
     else if(gameOutcome=="PaperPaper"){console.log("It's A tie you both picked " +computerSelection);
-    return tie}
+    return "tie"}
 
     else if(gameOutcome=="PaperScissors"){console.log("You Lose! " + computerSelection + " beats " + playerSelection);
-    return computerwin}
+    return "computerwin"}
 
     else if(gameOutcome=="PaperRock"){console.log("You Win! " + computerSelection + " beats " + playerSelection);
-    return playerwin}
+    return "playerwin"}
 
     else if(gameOutcome=="ScissorsPaper"){console.log("You Win! " + computerSelection + " beats " + playerSelection);
-    return playerwin}
+    return "playerwin"}
 
     else if(gameOutcome=="ScissorsScissors"){console.log("It's A tie you both picked " +computerSelection);
-    return tie}
+    return "tie"}
 
     else if(gameOutcome=="ScissorsRock"){console.log("You Lose! " + computerSelection + " beats " + playerSelection);
-    return computerwin}
+    return "computerwin"}
 
     else (console.log("Something went wrong! Pay no atention to the man behind the curtain"))
 }
@@ -43,7 +65,7 @@ function playRound(){
 function computerPlay(){
     let computerNum=randomNumber1to3();
     if(computerNum==1){
-        return1="Rock"
+        return "Rock"
     }  
     else if(computerNum==2){
         return "Paper"
