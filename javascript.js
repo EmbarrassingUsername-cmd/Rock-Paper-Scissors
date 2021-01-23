dbf9b2c77fa36a5d50f097569c97007f2f40b43b
Jamie Bukraba <Jamie.Bukraba@googlemail.com>
2021-01-23 13:00:08 +0000

    replaced computerPlay and playRound functions with arrays to remove unnessecary if statements

--- javascript.js
+++ javascript.js
@@ -8,7 +8,9 @@ function game(){
     for (i=0; i<totalRoundNum;i++){
         const playerSelection = toTitleCase(prompt("Rock Paper or Scissors? Round " + (1+i)));
         const computerSelection = computerPlay();
-        let result=playRound(playerSelection,computerSelection)
+        let gameOutcome=playerSelection+computerSelection
+        let result=playRound(gameOutcome,playerSelection,computerSelection)
+        
         if (result=="playerwin"){
             playerWin=++playerWin 
         }
@@ -21,7 +23,7 @@ function game(){
         else{}
     }
     if(playerWin>computerWin){
-        console.log("Congratulations you win! " + playerWin + " win(s) " + computerWin + "loss(es) and " + tieGame +" tie(s). Let's see if you can keep it up!");
+        console.log("Congratulations you win! " + playerWin + " win(s) " + computerWin + " loss(es) and " + tieGame +" tie(s). Let's see if you can keep it up!");
     }
     if(playerWin<computerWin){
         console.log("Sorry you lose " + playerWin + " win(s) " + computerWin + " loss(es) and " + tieGame + " tie(s). Better luck next time");
@@ -30,55 +32,22 @@ function game(){
         console.log("It's a tie " + playerWin + " win(s) " + computerWin + " loss(es) and " + tieGame + " tie(s) why not play again?" );
     }
     else{}
-
 }
-function playRound(playerSelection,computerSelection){
-    let gameOutcome=playerSelection+computerSelection
-    if (gameOutcome=="RockPaper"){console.log("You Lose! " + computerSelection + " beats " + playerSelection); 
-    return "computerwin"}
-
-    else if (gameOutcome=="RockScissors"){console.log("You Win! " + playerSelection + " beats " + computerSelection);
-    return "playerwin"}
-
-    else if(gameOutcome=="RockRock"){console.log("It's A tie you both picked " +computerSelection);
-    return "tie"}
-
-    else if(gameOutcome=="PaperPaper"){console.log("It's A tie you both picked " +computerSelection);
-    return "tie"}
-
-    else if(gameOutcome=="PaperScissors"){console.log("You Lose! " + computerSelection + " beats " + playerSelection);
-    return "computerwin"}
-
-    else if(gameOutcome=="PaperRock"){console.log("You Win! " + playerSelection + " beats " + computerSelection);
-    return "playerwin"}
-
-    else if(gameOutcome=="ScissorsPaper"){console.log("You Win! " + playerSelection + " beats " + computerSelection);
-    return "playerwin"}
-
-    else if(gameOutcome=="ScissorsScissors"){console.log("It's A tie you both picked " +computerSelection);
-    return "tie"}
-
-    else if(gameOutcome=="ScissorsRock"){console.log("You Lose! " + computerSelection + " beats " + playerSelection);
-    return "computerwin"}
-
-    else (console.log("Something went wrong! Pay no atention to the man behind the curtain"))
-}
+function playRound(gameOutcome,playerSelection,computerSelection){
+    const resultArray=["PaperRock","ScissorsPaper","RockScissors","PaperScissors","ScissorsRock","RockPaper","PaperPaper","ScissorsScissors","RockRock"]
+    let i=resultArray.indexOf(gameOutcome);
+    if(i<3){console.log("You Win! " + playerSelection + " beats " + computerSelection); return "playerwin"}
+    else if(i<6&&i>2){console.log("You Lose! " + computerSelection + " beats " + playerSelection); return "computerwin"}
+    else if(i>5){console.log("It's A tie you both picked " +computerSelection); return "tie"} 
+    }
+    
 
 function computerPlay(){
-    let computerNum=randomNumber1to3();
-    if(computerNum==1){
-        return "Rock"
-    }  
-    else if(computerNum==2){
-        return "Paper"
-    }
-    else{
-        return "Scissors"
-   
+    let compChoiceArray=["Rock","Paper","Scissors"]
+    return compChoiceArray[randomNumber(3)]
 }
-}
-function randomNumber1to3(){
-    return Math.floor(Math.random()*3+1)
+function randomNumber(x){
+    return Math.floor(Math.random()*x)
 }
 function toTitleCase(x){
     x=x.toLowerCase()
